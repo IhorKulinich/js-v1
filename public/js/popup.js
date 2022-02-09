c8789f9c6e42e2094b00867fd64492f90b66edc7
ruthenia-admin <coolgoodigor@gmail.com>
2022-02-09 13:15:06 +0200

    Update popup.js

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,275 +1,275 @@
-var Promise = TrelloPowerUp.Promise;
+// var Promise = TrelloPowerUp.Promise;
 
-var t = TrelloPowerUp.iframe();
+// var t = TrelloPowerUp.iframe();
 
-window.addEventListener("unhandledrejection", function (event) {
+// window.addEventListener("unhandledrejection", function (event) {
     
-  console.warn(event.reason);
+//   console.warn(event.reason);
   
-});
+// });
 
-function addoptbut(){
+// function addoptbut(){
     
-    var addoptb = document.createElement("button");
+//     var addoptb = document.createElement("button");
     
-    addoptb.setAttribute("id", "addoptbut");
+//     addoptb.setAttribute("id", "addoptbut");
     
-    document.getElementById("fieldsetting").appendChild(addoptb);
+//     document.getElementById("fieldsetting").appendChild(addoptb);
     
-    document.getElementById("addoptbut").innerHTML = "add option";
+//     document.getElementById("addoptbut").innerHTML = "add option";
     
-    document.getElementById("addoptbut").addEventListener("click", function(){
+//     document.getElementById("addoptbut").addEventListener("click", function(){
         
-        document.getElementById("addoptbut").style.display = "none";
+//         document.getElementById("addoptbut").style.display = "none";
         
-        addopttodiv();
+//         addopttodiv();
         
-    });
+//     });
     
-}
+// }
 
-function addopttodiv(){
+// function addopttodiv(){
     
-    var newlab = document.createElement("label");
+//     var newlab = document.createElement("label");
                 
-    var num = document.getElementById("fieldsetting").childNodes.filter(function(node){
+//     var num = document.getElementById("fieldsetting").childNodes.filter(function(node){
                    
-        return node.tag === "option";
+//         return node.tag === "option";
                     
-    }).length + 1;
+//     }).length + 1;
                 
-    newlab.setAttribute("id", "optlabel" + num);
+//     newlab.setAttribute("id", "optlabel" + num);
                 
-    newlab.setAttribute("for", "optval" + num);
+//     newlab.setAttribute("for", "optval" + num);
     
-    if (document.getElementById("addoptbut") === undefined || document.getElementById("addoptbut") === null){
+//     if (document.getElementById("addoptbut") === undefined || document.getElementById("addoptbut") === null){
                 
-        document.getElementById("fieldsetting").appendChild(newlab);
+//         document.getElementById("fieldsetting").appendChild(newlab);
     
-    } else {
+//     } else {
         
-        document.getElementById("fieldsetting").insertBefore(newlab, document.getElementById("addoptbut"));
+//         document.getElementById("fieldsetting").insertBefore(newlab, document.getElementById("addoptbut"));
         
-    }
+//     }
                 
-    document.getElementById("optlabel" + num).innerHTML = "add option value";
+//     document.getElementById("optlabel" + num).innerHTML = "add option value";
                 
-    var newopt = document.createElement("input");
+//     var newopt = document.createElement("input");
                 
-    newopt.setAttribute("type", "text");
+//     newopt.setAttribute("type", "text");
                 
-    newopt.setAttribute("id", "optval" + num);
+//     newopt.setAttribute("id", "optval" + num);
     
-    if (document.getElementById("addoptbut") === undefined || document.getElementById("addoptbut") === null){
+//     if (document.getElementById("addoptbut") === undefined || document.getElementById("addoptbut") === null){
                 
-        document.getElementById("fieldsetting").appendChild(newopt);
+//         document.getElementById("fieldsetting").appendChild(newopt);
     
-    } else {
+//     } else {
         
-        document.getElementById("fieldsetting").insertBefore(newopt, document.getElementById("addoptbut"));
+//         document.getElementById("fieldsetting").insertBefore(newopt, document.getElementById("addoptbut"));
         
-    }
+//     }
                 
-    document.getElementById("optval" + num).addEventListener("change", function(){
+//     document.getElementById("optval" + num).addEventListener("change", function(){
         
-        if (document.getElementById("addoptbut") === undefined || document.getElementById("addoptbut") === null){
+//         if (document.getElementById("addoptbut") === undefined || document.getElementById("addoptbut") === null){
                     
-            addoptbut();
+//             addoptbut();
         
-        } else {
+//         } else {
             
-            document.getElementById("addoptbut").style.display = "block";
+//             document.getElementById("addoptbut").style.display = "block";
             
-        } 
+//         } 
                     
-        if (num === 1){
+//         if (num === 1){
                     
-            window.settingtset.style.background = "green";
+//             window.settingtset.style.background = "green";
                     
-            window.settingset.addEventListener("submit", newparams.bind(null,data,i,id));
+//             window.settingset.addEventListener("submit", newparams.bind(null,data,i,id));
                     
-        }
+//         }
                     
-    });
+//     });
     
-}
+// }
 
-function setfieldpar(data,i,id,t){
+// function setfieldpar(data,i,id,t){
     
-    function fsback(){
+//     function fsback(){
         
-        document.getElementById("fieldsetting").style.display = "none";
+//         document.getElementById("fieldsetting").style.display = "none";
     
-        document.getElementById("fieldsgroup").style.display = "block";
+//         document.getElementById("fieldsgroup").style.display = "block";
         
-        document.getElementById("fsback").removeEventListener("click", fsback);
+//         document.getElementById("fsback").removeEventListener("click", fsback);
         
-        document.getElementById("fback").addEventListener("click",fback);
+//         document.getElementById("fback").addEventListener("click",fback);
         
-    }
+//     }
     
-    document.getElementById("fsback").addEventListener("click",fsback);
+//     document.getElementById("fsback").addEventListener("click",fsback);
     
-    document.getElementById("fieldsgroup").style.display = "none";
+//     document.getElementById("fieldsgroup").style.display = "none";
     
-    document.getElementById("fieldsetting").style.display = "block";
+//     document.getElementById("fieldsetting").style.display = "block";
     
-    switch(data[i].type){
+//     switch(data[i].type){
                 
-        case "text":
+//         case "text":
                     
-            document.getElementById("textsettings").style.display="block";
+//             document.getElementById("textsettings").style.display="block";
                     
-            document.getElementById("nameset").value = data.name;
+//             document.getElementById("nameset").value = data.name;
     
-            document.getElementById("typeset").value = "text";
+//             document.getElementById("typeset").value = "text";
             
-            document.getElementById("nameset").addEventListener('input', function(){
+//             document.getElementById("nameset").addEventListener('input', function(){
                 
-                window.settingtset.style.background = "green";
+//                 window.settingtset.style.background = "green";
                 
-                window.settingset.addEventListener("submit", newparams.bind(null,data,i,id,t));
+//                 window.settingset.addEventListener("submit", newparams.bind(null,data,i,id,t));
                 
-            });
+//             });
             
-            document.getElementById("typeset").addEventListener('change', function(){
+//             document.getElementById("typeset").addEventListener('change', function(){
                 
-                addopttodiv();
+//                 addopttodiv();
                 
-            });
+//             });
                 
-            break;
+//             break;
                 
-        case "list":
+//         case "list":
                     
-            document.getElementById("listsettings").style.display="block";
+//             document.getElementById("listsettings").style.display="block";
                 
-        break;
+//         break;
                 
-    }
+//     }
     
-}
+// }
 
-function newparams(data,i,id,t){
+// function newparams(data,i,id,t){
     
-    var ind;
+//     var ind;
     
-    var changing = data.filter(function(id,ind,field,index){
+//     var changing = data.filter(function(id,ind,field,index){
         
-        if (field.id === id) { ind = index; } 
+//         if (field.id === id) { ind = index; } 
         
-    }.bind(null,id,ind))[0];
+//     }.bind(null,id,ind))[0];
     
-    if (changing.name != document.getElementById("nameset").value){
+//     if (changing.name != document.getElementById("nameset").value){
         
-        data[ind].name = document.getElementById("nameset").value;
+//         data[ind].name = document.getElementById("nameset").value;
         
-    }
+//     }
     
-    if (changing.type != document.getElementById("typeset").value){
+//     if (changing.type != document.getElementById("typeset").value){
         
-        data[ind].type = document.getElementById("typeset").value;
+//         data[ind].type = document.getElementById("typeset").value;
         
-        switch(data[ind].type){
+//         switch(data[ind].type){
             
-            case "text":
+//             case "text":
                 
-                delete data[ind].values;
+//                 delete data[ind].values;
                 
-                break;
+//                 break;
                 
-            case "list":
+//             case "list":
                 
-                data[ind].values = [];
+//                 data[ind].values = [];
                 
-                var num = document.getElementById("fieldsetting").childNodes.filter(function(node){
+//                 var num = document.getElementById("fieldsetting").childNodes.filter(function(node){
                    
-                    return node.tag === "option";
+//                     return node.tag === "option";
                     
-                });
+//                 });
                 
-                for (var j=0; j<num-1; j++){
+//                 for (var j=0; j<num-1; j++){
                 
-                    data[ind].values.push(document.getElementById("optval" + j).value); 
+//                     data[ind].values.push(document.getElementById("optval" + j).value); 
                 
-                }
+//                 }
                 
-                break;
+//                 break;
             
-        }
+//         }
         
-    }
+//     }
     
-    var datar = {"fields": data};
+//     var datar = {"fields": data};
     
-    return t.set('board', 'shared', 'fieldset', datar)
+//     return t.set('board', 'shared', 'fieldset', datar)
     
-    .then(function(){
+//     .then(function(){
             
-        document.getElementById("fieldsetting").style.display = "none";
+//         document.getElementById("fieldsetting").style.display = "none";
         
-        document.getElementById("fieldsgroup").style.display = "block";
+//         document.getElementById("fieldsgroup").style.display = "block";
             
-    });
+//     });
     
-}
+// }
 
-//settingsgroup //fieldsgroup //fieldsetting //textsettings, settingtset
+// //settingsgroup //fieldsgroup //fieldsetting //textsettings, settingtset
     
-t.render(function(t){
+// t.render(function(t){
         
-    document.getElementById("fieldssettings").addEventListener("click", function(){
+//     document.getElementById("fieldssettings").addEventListener("click", function(){
     
-        document.getElementById("fieldsgroup").style.display = "block";
+//         document.getElementById("fieldsgroup").style.display = "block";
         
-        console.log("fields menu: "+document.getElementById("fieldsgroup").style.display);
+//         console.log("fields menu: "+document.getElementById("fieldsgroup").style.display);
         
-        document.getElementById("settingsgroup").style.display = "none";
+//         document.getElementById("settingsgroup").style.display = "none";
         
-        function fback(){
+//         function fback(){
             
-            document.getElementById("settingsgroup").style.display = "block";
+//             document.getElementById("settingsgroup").style.display = "block";
         
-            document.getElementById("fieldsgroup").style.display = "none";
+//             document.getElementById("fieldsgroup").style.display = "none";
             
-            document.getElementById("fback").removeEventListener("click", fback);
+//             document.getElementById("fback").removeEventListener("click", fback);
             
-        }
+//         }
         
-        document.getElementById("fback").addEventListener("click",fback);
+//         document.getElementById("fback").addEventListener("click",fback);
             
-        return t.get('board', 'shared', 'fieldset')
+//         return t.get('board', 'shared', 'fieldset')
         
-        .then(function(datar){
+//         .then(function(datar){
             
-            console.log(datar);
+//             console.log(datar);
             
-            var data = datar.fields;
+//             var data = datar.fields;
 
-            console.log(data);
+//             console.log(data);
             
-            for (var i=0; i<data.length; i++){
+//             for (var i=0; i<data.length; i++){
                 
-                var field = document.createElement("button");
+//                 var field = document.createElement("button");
                 
-                field.setAttribute("id", "fieldset"+data.id);
+//                 field.setAttribute("id", "fieldset"+data.id);
                 
-                document.getElementById("fieldsgroup").appendChild(field);
+//                 document.getElementById("fieldsgroup").appendChild(field);
                 
-                document.getElementById("fieldset"+data.id).innerHTML = "<p style='display: inline;'>" + data[i].name + "</p><p style='text-align: right; display: inline;'>></p>";
+//                 document.getElementById("fieldset"+data.id).innerHTML = "<p style='display: inline;'>" + data[i].name + "</p><p style='text-align: right; display: inline;'>></p>";
                 
-                document.getElementById("fieldset"+data.id).addEventListener("click", setfieldpar.bind(null,data,i,data[i].id,t));
+//                 document.getElementById("fieldset"+data.id).addEventListener("click", setfieldpar.bind(null,data,i,data[i].id,t));
                     
-            }
+//             }
             
-        })
+//         })
         
-        .then(function(){
+//         .then(function(){
             
-            t.sizeTo(document.body);
+//             t.sizeTo(document.body);
             
-        });
+//         });
       
-    });
+//     });
 
-}.bind(null,t));
+// }.bind(null,t));
