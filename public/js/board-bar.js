f7d4df235b4a1b4931bee21b1f68ed8e70dd138a
ruthenia-admin <coolgoodigor@gmail.com>
2022-02-09 23:35:21 +0200

    Update board-bar.js

--- public/js/board-bar.js
+++ public/js/board-bar.js
@@ -7,277 +7,279 @@ var fieldset = document.getElementById("addfieldset");
 
 var adder = () => {
 
-  document.getElementById("addfield").style.display = 'none';
+  // document.getElementById("addfield").style.display = 'none';
 
-  var newfielddata;
+  // var newfielddata;
   
-  var label = document.createElement("label");
+  // var label = document.createElement("label");
 
-  label.setAttribute("for", "typefield");
+  // label.setAttribute("for", "typefield");
 
-  label.setAttribute("id", "typelabel");
+  // label.setAttribute("id", "typelabel");
 
-  fieldset.appendChild(label);
+  // fieldset.appendChild(label);
 
-  document.getElementById("typelabel").innerHTML = "choose type";
+  // document.getElementById("typelabel").innerHTML = "choose type";
 
-  var typefield = document.createElement("input");
+  // var typefield = document.createElement("input");
 
-  typefield.setAttribute("id", "typefield");
+  // typefield.setAttribute("id", "typefield");
 
-  typefield.setAttribute("list", "typelist");
+  // typefield.setAttribute("list", "typelist");
 
-  var settype = document.createElement("datalist");
+  // var settype = document.createElement("datalist");
 
-  settype.setAttribute("id", "typelist");
+  // settype.setAttribute("id", "typelist");
 
-  fieldset.appendChild(typefield);
+  // fieldset.appendChild(typefield);
 
-  fieldset.appendChild(settype);
+  // fieldset.appendChild(settype);
 
-  var typeoption = document.createElement("option");
+  // var typeoption = document.createElement("option");
 
-  typeoption.setAttribute("value", "text");
+  // typeoption.setAttribute("value", "text");
 
-  typeoption.setAttribute("id", "optiontext");
+  // typeoption.setAttribute("id", "optiontext");
 
-  document.getElementById("typelist").appendChild(typeoption);
+  // document.getElementById("typelist").appendChild(typeoption);
   
-  typeoption = document.createElement("option");
+  // typeoption = document.createElement("option");
 
-  typeoption.setAttribute("value", "list");
+  // typeoption.setAttribute("value", "list");
 
-  typeoption.setAttribute("id", "optionlist");
+  // typeoption.setAttribute("id", "optionlist");
 
-  document.getElementById("typelist").appendChild(typeoption);
+  // document.getElementById("typelist").appendChild(typeoption);
 
-  var settypefield = () => {
+  // var settypefield = () => {
 
-    switch(document.getElementById("typefield").value){
+  //   switch(document.getElementById("typefield").value){
         
-      case "text":
+  //     case "text":
 
-        console.log(document.getElementById("butdiv"));
+  //       console.log(document.getElementById("butdiv"));
 
-        console.log(document.getElementById("submitter"));
+  //       console.log(document.getElementById("submitter"));
 
-        var date = Date.now();
+  //       var date = Date.now();
 
-        newfielddata = {"type": "text", "name": null, "id": date, "value": null};
+  //       newfielddata = {"type": "text", "name": null, "id": date, "value": null};
 
-        fieldset.childNodes.forEach((node) => node.id != "butdiv" ? node.remove() : null);
+  //       fieldset.childNodes.forEach((node) => node.id != "butdiv" ? node.remove() : null);
 
-        document.getElementById("typelabel").remove();
+  //       document.getElementById("typelabel").remove();
         
-        var label = document.createElement("label");
+  //       var label = document.createElement("label");
 
-        label.setAttribute("for", "namefield");
+  //       label.setAttribute("for", "namefield");
 
-        label.setAttribute("id", "namelabel1");
+  //       label.setAttribute("id", "namelabel1");
         
-        console.log(label);
+  //       console.log(label);
 
-        console.log(document.getElementById("submiter"));
+  //       console.log(document.getElementById("submiter"));
 
-        document.getElementById("butdiv").insertBefore(label, document.getElementById("submiter"));
+  //       document.getElementById("butdiv").insertBefore(label, document.getElementById("submiter"));
         
-        document.getElementById("namelabel1").innerHTML = "choose name";
+  //       document.getElementById("namelabel1").innerHTML = "choose name";
 
-        var namefield = document.createElement("input");
+  //       var namefield = document.createElement("input");
 
-        namefield.setAttribute("type", "text");
+  //       namefield.setAttribute("type", "text");
 
-        namefield.setAttribute("id", "namefield");
+  //       namefield.setAttribute("id", "namefield");
         
-        document.getElementById("butdiv").insertBefore(namefield, document.getElementById("submiter"));
+  //       document.getElementById("butdiv").insertBefore(namefield, document.getElementById("submiter"));
         
-        var addname = () => {
+  //       var addname = () => {
 
-          newfielddata.name = document.getElementById("namefield").value;
+  //         newfielddata.name = document.getElementById("namefield").value;
 
-          console.log(newfielddata);
+  //         console.log(newfielddata);
 
-        };
+  //       };
 
-        document.getElementById("namefield").addEventListener("change", addname); 
+  //       document.getElementById("namefield").addEventListener("change", addname); 
 
-        break;
+  //       break;
 
-    case "list":
+  //   case "list":
 
-      console.log(document.getElementById("butdiv"));
+  //     console.log(document.getElementById("butdiv"));
 
-      console.log(document.getElementById("submitter"));
+  //     console.log(document.getElementById("submitter"));
 
-      var date = Date.now();
+  //     var date = Date.now();
 
-      newfielddata = {"type": "list", "name": null, "id": date, "value": null, "values": []};
+  //     newfielddata = {"type": "list", "name": null, "id": date, "value": null, "values": []};
 
-      fieldset.childNodes.forEach((node) =>  node.id != "butdiv" ? node.remove() : null);
+  //     fieldset.childNodes.forEach((node) =>  node.id != "butdiv" ? node.remove() : null);
 
-      document.getElementById("typelabel").remove();
+  //     document.getElementById("typelabel").remove();
       
-      var label = document.createElement("label");
+  //     var label = document.createElement("label");
 
-      label.setAttribute("for", "namefield2");
+  //     label.setAttribute("for", "namefield2");
 
-      label.setAttribute("id", "namelabel2");
+  //     label.setAttribute("id", "namelabel2");
       
-      document.getElementById("butdiv").insertBefore(label, document.getElementById("submiter"));
+  //     document.getElementById("butdiv").insertBefore(label, document.getElementById("submiter"));
       
-      document.getElementById("namelabel2").innerHTML = "choose name";
+  //     document.getElementById("namelabel2").innerHTML = "choose name";
 
-      namefield = document.createElement("input");
+  //     namefield = document.createElement("input");
 
-      namefield.setAttribute("type", "text");
+  //     namefield.setAttribute("type", "text");
 
-      namefield.setAttribute("id", "namefield2");
+  //     namefield.setAttribute("id", "namefield2");
       
-      document.getElementById("butdiv").insertBefore(namefield, document.getElementById("submiter"));
+  //     document.getElementById("butdiv").insertBefore(namefield, document.getElementById("submiter"));
       
-      var addname = () => {
+  //     var addname = () => {
 
-        newfielddata.name = document.getElementById("namefield2").value;
+  //       newfielddata.name = document.getElementById("namefield2").value;
 
-        console.log(newfielddata);
+  //       console.log(newfielddata);
 
-      };
+  //     };
 
-      document.getElementById("namefield2").addEventListener("change", addname);
+  //     document.getElementById("namefield2").addEventListener("change", addname);
 
-      label = document.createElement("label");
+  //     label = document.createElement("label");
 
-      label.setAttribute("for", "valuefield");
+  //     label.setAttribute("for", "valuefield");
 
-      label.setAttribute("id", "valuelabel");
+  //     label.setAttribute("id", "valuelabel");
       
-      document.getElementById("butdiv").insertBefore(label, document.getElementById("submiter"));
+  //     document.getElementById("butdiv").insertBefore(label, document.getElementById("submiter"));
       
-      document.getElementById("valuelabel").innerHTML = "choose value";
+  //     document.getElementById("valuelabel").innerHTML = "choose value";
 
-      var valuefield = document.createElement("input");
+  //     var valuefield = document.createElement("input");
 
-      valuefield.setAttribute("type", "text");
+  //     valuefield.setAttribute("type", "text");
 
-      valuefield.setAttribute("id", "valuefield");
+  //     valuefield.setAttribute("id", "valuefield");
       
-      document.getElementById("butdiv").insertBefore(valuefield, document.getElementById("submiter"));
+  //     document.getElementById("butdiv").insertBefore(valuefield, document.getElementById("submiter"));
       
-      var addvalue = () => {
+  //     var addvalue = () => {
 
-        newfielddata.values.push(document.getElementById("valuefield").value);
+  //       newfielddata.values.push(document.getElementById("valuefield").value);
 
-        console.log(newfielddata);
+  //       console.log(newfielddata);
 
-        document.getElementById("valuefield").setAttribute("value", null);
+  //       document.getElementById("valuefield").setAttribute("value", null);
 
-      };
+  //     };
 
-      document.getElementById("valuefield").addEventListener("change", addvalue);
+  //     document.getElementById("valuefield").addEventListener("change", addvalue);
       
-      var addoption1 = document.createElement("button");
+  //     var addoption1 = document.createElement("button");
 
-      addoption1.setAttribute("id", "addoption1");
+  //     addoption1.setAttribute("id", "addoption1");
 
-      document.getElementById("butdiv").insertBefore(addoption1, document.getElementById("submiter"));//appendChild(addoption1); //fieldset.insertBefore(namefield, document.getElementById("submiter"));
+  //     document.getElementById("butdiv").insertBefore(addoption1, document.getElementById("submiter"));//appendChild(addoption1); //fieldset.insertBefore(namefield, document.getElementById("submiter"));
       
-      document.getElementById("addoption1").innerHTML = "add option";
+  //     document.getElementById("addoption1").innerHTML = "add option";
         
-      var nextoption = () => {
+  //     var nextoption = () => {
 
-        if (document.getElementById("valuefield").value !== null ? document.getElementById("valuefield").value.length > 0 : false){
+  //       if (document.getElementById("valuefield").value !== null ? document.getElementById("valuefield").value.length > 0 : false){
 
-          document.getElementById("valuefield").value = "";
+  //         document.getElementById("valuefield").value = "";
 
-        }
+  //       }
 
-      };
+  //     };
 
-      document.getElementById("addoption1").addEventListener("click", nextoption);
+  //     document.getElementById("addoption1").addEventListener("click", nextoption);
 
-      break;
+  //     break;
 
-    }
+  //   }
 
-  };
+  // };
 
-  document.getElementById("typefield").addEventListener("change", settypefield);
+  // document.getElementById("typefield").addEventListener("change", settypefield);
 
-  var butdiv = document.createElement("div");
+  // var butdiv = document.createElement("div");
 
-  butdiv.setAttribute("style", "width: 100%; display: grid; grid-template-columns: calc( 50% - 2px ) calc( 50% - 2px ); grid-column-gap: 4px;");
+  // butdiv.setAttribute("style", "width: 100%; display: grid; grid-template-columns: calc( 50% - 2px ) calc( 50% - 2px ); grid-column-gap: 4px;");
   
-  butdiv.setAttribute("id", "butdiv");
+  // butdiv.setAttribute("id", "butdiv");
 
-  fieldset.appendChild(butdiv);
+  // fieldset.appendChild(butdiv);
   
-  var submiter = document.createElement("button");
+  // var submiter = document.createElement("button");
 
-  submiter.setAttribute("style", "background: green;");
+  // submiter.setAttribute("style", "background: green;");
 
-  submiter.setAttribute("id", "submiter");
+  // submiter.setAttribute("id", "submiter");
 
-  document.getElementById("butdiv").appendChild(submiter);
+  // document.getElementById("butdiv").appendChild(submiter);
 
-  document.getElementById("submiter").innerHTML = "submit";
+  // document.getElementById("submiter").innerHTML = "submit";
   
-  var submit = () => {
+  // var submit = () => {
 
-    event.preventDefault();
+  //   event.preventDefault();
 
-    return t.get('board', 'shared', 'fieldset')
+  //   return t.get('board', 'shared', 'fieldset')
 
-    .then(function(data){
+  //   .then(function(data){
 
-        console.log(data);
+  //       console.log(data);
 
-        console.log("fields in data:");
+  //       console.log("fields in data:");
 
-        console.log(data.hasOwnProperty("fields"));
+  //       console.log(data.hasOwnProperty("fields"));
 
-        if (!data.hasOwnProperty("fields")){
+  //       if (!data.hasOwnProperty("fields")){
 
-            data = {"fields": []};
+  //           data = {"fields": []};
 
-            console.log(data);
+  //           console.log(data);
 
-        }
+  //       }
 
-        data.fields.push(newfielddata);
+  //       data.fields.push(newfielddata);
 
-        console.log(data);
+  //       console.log(data);
 
-        return t.set('board', 'shared', 'fieldset', data)
+  //       return t.set('board', 'shared', 'fieldset', data)
 
-        .then(function(){
+  //       .then(function(){
 
-            fieldset.childNodes.forEach((node) => node.remove());
+  //           fieldset.childNodes.forEach((node) => node.remove());
 
-            document.getElementById("submiter").remove();
+  //           document.getElementById("submiter").remove();
 
-            document.getElementById("addfield").style.display = 'block';
+  //           document.getElementById("addfield").style.display = 'block';
 
-            var valuelabel = document.getElementById("valuelabel");
+  //           var valuelabel = document.getElementById("valuelabel");
 
-            if (valuelabel){
+  //           if (valuelabel){
 
-                valuelabel.remove();
+  //               valuelabel.remove();
 
-                document.getElementById("namelabel2").remove();
+  //               document.getElementById("namelabel2").remove();
 
-            } else {
+  //           } else {
 
-                document.getElementById("namelabel").remove();
+  //               document.getElementById("namelabel").remove();
 
-            }
+  //           }
             
-        })
+  //       })
 
-    });
+  //   });
 
-  }
+  // }
 
-  window.addfieldset.addEventListener("submit", submit);
+  // window.addfieldset.addEventListener("submit", submit);
+
+  document.getElementById("textsettingsb").style.display = "";
 
 }
 
